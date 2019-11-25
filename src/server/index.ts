5a9000d7c36b5c1e3528d074737c740afd3a9648
MattMan569 <matthewpolsom@hotmail.com>
2019-11-25 17:04:45 -0600

    basic form rendering

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { Request, Response } from "express";
+import fs from "fs";
 import hbs from "hbs";
 import path from "path";
-import { Result } from "range-parser";
 
 const websiteTitle = "Chat App";
 const websiteAuthor = "Matthew Polsom";
@@ -11,9 +11,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Define paths
-const publicDirectoryPath = path.join(__dirname, "./../client");
+const publicDirectoryPath = path.join(__dirname, "../client");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Get the partials to send to the client
+const clientPartials = {
+    login: fs.readFileSync(path.join(__dirname, "../templates/partials/login.hbs"), "utf-8"),
+    signup: fs.readFileSync(path.join(__dirname, "../templates/partials/signup.hbs"), "utf-8"),
+};
 
 // Setup hbs
 app.set("view engine", "hbs");
@@ -23,11 +29,16 @@ hbs.registerPartials(partialsPath);
 // Set the static directory
 app.use(express.static(publicDirectoryPath));
 
+app.get("/partialForms", (req: Request, res: Response) => {
+    res.send(clientPartials);
+});
+
 // Login / signup
 app.get("/", (req: Request, res: Response) => {
     res.render("index", {
         websiteAuthor,
         websiteTitle,
+
     });
 });
 
@@ -51,5 +62,3 @@ app.get("/chat", (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
-
-export { partialsPath };
