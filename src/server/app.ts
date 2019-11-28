17350e88dbc7de27aeeb6c13b59ce933d29dbc7d
MattMan569 <matthewpolsom@hotmail.com>
2019-11-28 00:18:05 -0600

    setup session management

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,11 @@
+import connectMongo from "connect-mongo";
 import cors from "cors";
 import express from "express";
+import session from "express-session";
 import hbs from "hbs";
+import mongoose from "mongoose";
 import path from "path";
+import uuidv4 from "uuid/v4";
 require("./db/mongoose");
 import chatRouter from "./routers/chat";
 import userRouter from "./routers/user";
@@ -15,9 +19,29 @@ const partialsPath = path.join(__dirname, "./templates/partials");
 const app = express();
 
 // Use CORS
+// TODO: check if needed
 app.use(cors({
     credentials: true,
     origin: true,
+}));
+
+// Automatically parse json
+app.use(express.json());
+
+// Process HTML POST requests
+app.use(express.urlencoded({
+    extended: false,
+}));
+
+// Setup session management
+const MongoStore = connectMongo(session);
+app.use(session({
+    genid: (req) => uuidv4(),
+    resave: false,
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+    }),
 }));
 
 // Setup hbs
@@ -25,14 +49,17 @@ app.set("view engine", "hbs");
 app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
-// Automatically parse json
-app.use(express.json());
-
 // Set the static directory
 app.use(express.static(publicDirectoryPath));
 
 // Setup routers
 app.use(chatRouter);
 app.use(userRouter);
+
+// TODO
+// Global 404
+// app.get("*", (req: Request, res: Response) => {
+//
+// });
 
 export default app;
