163222810d5a5f6e9cb3889bd3b843a03b4cfc0c
MattMan569 <matthewpolsom@hotmail.com>
2020-01-09 16:57:07 -0600

    create room router

---
+++ src/server/routers/room.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+import auth from "../middleware/auth";
+import Room, { IRoom } from "../models/room";
+
+const router = express.Router();
+
+// Create a room
+router.post("/rooms/create", async (req: Request, res: Response) => {
+
+});
