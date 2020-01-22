519b5457b38c00bb690b380b420d8fea8c72a608
MattMan569 <matthewpolsom@hotmail.com>
2020-01-21 18:21:11 -0600

    moves typings to a new root folder

---
+++ src/types/types.d.ts
@@ -0,0 +1,18 @@
+import { IUser } from "../server/models/user";
+
+// Extend the cookie to include the user
+declare global {
+    namespace Express {
+        // tslint:disable-next-line: interface-name
+        interface Session {
+            user: IUser;
+        }
+    }
+}
+
+// Socket.io message format
+export interface ISocketIOMessage {
+    sender: string;
+    message: string;
+    createdAt: Date;
+}
