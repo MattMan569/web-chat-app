63cfa2a5eefcaa3ef83ff791a20e553a1dfb8a53
MattMan569 <matthewpolsom@hotmail.com>
2020-01-23 21:51:41 -0600

    solve deprecation warning

--- src/server/db/mongoose.ts
+++ src/server/db/mongoose.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.connect(process.env.MONGODB_URL, {
     useCreateIndex: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
