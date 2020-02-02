7df01fe18a862258b95ce364a004c63fc0eebb9a
MattMan569 <matthewpolsom@hotmail.com>
2020-02-02 13:40:25 -0600

    send messages with enter, newlines with shift + enter

--- src/client/ts/chat.ts
+++ src/client/ts/chat.ts
@@ -6,6 +6,7 @@ import { ISocketIOMessage } from "./../../types/types";
 
 const socket = socketio("/chat");
 
+const messageForm = $("#message-form");
 const formControls = $("#form-controls");
 const messageField = $("#message-field");
 
@@ -13,7 +14,7 @@ const template = $("#message-item-template").html();
 const render = Handlebars.compile(template);
 const messageList = $("#message-list");
 
-$("#message-form").submit((e) => {
+const submitMessageForm = (e: JQuery.SubmitEvent) => {
     e.preventDefault();
     formControls.attr("disabled", "disabled");
     const message = messageField.val() as string;
@@ -23,7 +24,29 @@ $("#message-form").submit((e) => {
         messageField.val("");
         messageField.focus();
     });
+};
+
+messageForm.submit((e) => {
+    submitMessageForm(e);
 });
+
+messageForm.keypress((e) => {
+    if (e.which === 13 && !e.shiftKey) {
+        messageForm.submit();
+    }
+});
+
+// $("#message-form").submit((e) => {
+//     e.preventDefault();
+//     formControls.attr("disabled", "disabled");
+//     const message = messageField.val() as string;
+
+//     socket.emit("sendMessage", message, () => {
+//         formControls.removeAttr("disabled");
+//         messageField.val("");
+//         messageField.focus();
+//     });
+// });
 
 socket.on("message", (message: ISocketIOMessage) => {
     messageList.append(render(message));
