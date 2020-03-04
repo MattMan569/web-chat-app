ef1d701517a0704ab961f0b6044f0c0fc29b5ec7
MattMan569 <matthewpolsom@hotmail.com>
2020-03-04 13:11:05 -0600

    store avatar locally and get from server on load

--- src/client/ts/common.ts
+++ src/client/ts/common.ts
@@ -1,8 +1,38 @@
+import $ from "jquery";
+import localforage from "localforage";
+import { IUser } from "../../server/models/user";
+
 // Code common among all pages
 // Header, footer, etc.
 
 const profileButton = document.getElementById("profile-btn");
 const profileDropdownContent = document.getElementById("profile-dropdown-content");
+
+// First get the stored avatar for a more responsive UI
+localforage.getItem("avatar", (err, avatar) => {
+    if (err) {
+        return console.log(err);
+    }
+    $("#profile-btn").css("background-image", `url(data:image/png;base64,${avatar})`);
+});
+
+// Get the avatar stored on the server
+$.ajax({
+    url: "/users/me",
+    method: "GET",
+    cache: true,
+    statusCode: {
+        200: (me: IUser) => {
+            // Store the avatar client-side
+            localforage.setItem("avatar", me.avatar, (err, avatar) => {
+                if (err) {
+                    return console.log(err);
+                }
+                $("#profile-btn").css("background-image", `url(data:image/png;base64,${avatar})`);
+            });
+        },
+    },
+});
 
 // Add an event listener if the profile button is present
 profileButton?.addEventListener("click", (e: MouseEvent) => {
