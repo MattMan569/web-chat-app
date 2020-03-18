6c11a037146daeec928de6e75457449114149b53
MattMan569 <matthewpolsom@hotmail.com>
2020-03-17 21:39:15 -0600

    remove todo

--- src/server/routers/util/routerOptions.ts
+++ src/server/routers/util/routerOptions.ts
@@ -7,8 +7,6 @@ export const getRouterOptions = (req: Express.Request, pageTitle: string) => {
     return {
         pageTitle,
         loggedIn: req.session.loggedIn,
-        // TODO just use the whole user obj
-        // user: req.session.user,
         username: req.session.user?.username,
         userId: req.session.user?._id,
         ...websiteInfo,
